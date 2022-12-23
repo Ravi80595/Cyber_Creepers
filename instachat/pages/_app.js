410a771b495b9bd8fd6588eda9949e96ef497934
AbhayAg10 <abhaytalkin@gmail.com>
2022-12-23 11:47:15 +0530

    feed

--- instachat/pages/_app.js
+++ instachat/pages/_app.js
@@ -3,16 +3,20 @@ import Navbar from '../Components/Navbar'
 import { ChakraProvider } from '@chakra-ui/react'
 import AdminSign from '../Components/AdminSign'
 import AdminLogin from '../Components/AdminLogin'
+import Home from './Home'
+import Post from './Post'
 // import Dashboard from "./Admin/Dashboard"
 
 export default function App({ Component, pageProps }) {
-  return<>
+  return <>
   <ChakraProvider>
-  <Navbar/>
-  {/* <AdminSign/> */}
-  {/* <AdminLogin/> */}
-  {/* <Dashboard/> */}
-   <Component {...pageProps} />
+    <Navbar/>
+    {/* <AdminSign/> */}
+    {/* <AdminLogin/> */}
+    {/* <Dashboard/> */}
+    {/* <Home /> */}
+    <Post />
+    {/* <Component {...pageProps} /> */}
    </ChakraProvider>
-   </>
+  </>
 }
